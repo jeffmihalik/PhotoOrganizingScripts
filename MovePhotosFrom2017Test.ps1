bbb38321e065a8d41625b66674cee6b936888180
Jeff <jeff.mihalik@gmail.com>
2021-10-15 22:04:16 -0400

    It works!  Moves all photos from source directory taken in 2017 to the output directory

--- MovePhotosFrom2017Test.ps1
+++ MovePhotosFrom2017Test.ps1
@@ -1,60 +1,38 @@
-# [DateTime]$start_time="2017-1-1 00:00:00"
-# [DateTime]$end_time="2017-12-31 23:59:59"
-# $dest_folder = "D:\Photos\test output\2017"
-# $source_folder = "D:\Photos\test"
-
-
-# Get-ChildItem -Path $source_folder -recurse -include @("*.mov") | Where-Object { $_.CreationTime -ge "03/01/2017" -and $_.CreationTime -le "03/31/2018" }
-
-# Get-ChildItem D:\Photos\test\*.MOV Where-Object { $_.CreationTime -ge $start_time -and $_.CreationTime -le $end_time }
-
-# | foreach {if($_.CreationTime -ge $start_time -and $_.CreationTime -le $end_time) { move-item $_.fullname $des_folder }}
-
-
-# $fldPath = "D:\Photos\test"
-# $flExt = ".mov";
-# $attrName = "media created"
-# (Get-ChildItem -Path "$fldPath\*" -Include "*$flExt").FullName | % { 
-#     $path = $_
-#     $shell = New-Object -COMObject Shell.Application;
-#     $folder = Split-Path $path;
-#     $file = Split-Path $path -Leaf;
-#     $shellfolder = $shell.Namespace($folder);
-#     $shellfile = $shellfolder.ParseName($file);
-
-#     $a = 0..500 | % { Process { $x = '{0} = {1}' -f $_, $shellfolder.GetDetailsOf($null, $_); If ( $x.split("=")[1].Trim() ) { $x } } };
-#     [int]$num = $a | % { Process { If ($_ -like "*$attrName*") { $_.Split("=")[0].trim() } } };
-#     $mCreated = $shellfolder.GetDetailsOf($shellfile, $num);
-#     $mCreated;
-# };
-
 $dest_folder = "D:\Photos\test output\2017folder\"
 $source_folder = "D:\Photos\test"
 
 [DateTime]$startTime="2017-1-1 00:00:00"
 [DateTime]$endTime="2017-12-31 23:59:59"
-$flPath = "D:\Photos\test\IMG_0977.mov";
 $attrName = "media created"
-$path = $flPath;
-$shell = New-Object -COMObject Shell.Application;
-$folder = Split-Path $path;
-$file = Split-Path $path -Leaf;
-$shellfolder = $shell.Namespace($folder);
-$shellfile = $shellfolder.ParseName($file);
+$fldPath = "D:\Photos\test"
+$flExt = ".mov";
 
-$mCreated = $shellfolder.GetDetailsOf($shellfile, 208);
-$parsedDate = ($mCreated -replace [char]8206) -replace [char]8207
-$provider = New-Object System.Globalization.CultureInfo "en-US"
+(Get-ChildItem -Path "$fldPath\*" -Include "*$flExt").FullName | foreach { 
 
-$dateTime = [datetime]::ParseExact($parsedDate, 'M/d/yyyy h:mm tt', $provider)
-if ($startTime -le $dateTime) {
-    if ($dateTime -le $endTime)
-    {
-        Write-Output "in date range" + $shellFile
-        move-item $flPath $dest_folder
+    $path = $_
+    $shell = New-Object -COMObject Shell.Application;
+    $folder = Split-Path $path;
+    $file = Split-Path $path -Leaf;
+    $shellfolder = $shell.Namespace($folder);
+    $shellfile = $shellfolder.ParseName($file);
+
+    $a = 0..500 | % { Process { $x = '{0} = {1}' -f $_, $shellfolder.GetDetailsOf($null, $_); If ( $x.split("=")[1].Trim() ) { $x } } };
+    [int]$num = $a | % { Process { If ($_ -like "*$attrName*") { $_.Split("=")[0].trim() } } };
+    $mCreated = $shellfolder.GetDetailsOf($shellfile, $num);
+    $mCreated;
+    $parsedDate = ($mCreated -replace [char]8206) -replace [char]8207
+    $provider = New-Object System.Globalization.CultureInfo "en-US"
+
+    $dateTime = [datetime]::ParseExact($parsedDate, 'M/d/yyyy h:mm tt', $provider)
+    if ($startTime -le $dateTime) {
+        if ($dateTime -le $endTime)
+        {
+            Write-Output "in date range" + $shellFile
+            move-item $path $dest_folder
+        }
     }
-}
-else {
-    $dateTime;
-    Write-Output "not in date range"
-}
+    else {
+        $dateTime;
+        Write-Output "not in date range"
+    }
+};
